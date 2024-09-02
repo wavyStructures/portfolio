97d2ce9c6abb900499846cd02ce7ab0d7cc032d1
wavyStructures <anja_schwab@gmx.de>
2024-09-02 12:11:12 +0200

    creating components

--- src/app/contactform/contactform.component.ts
+++ src/app/contactform/contactform.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contactform',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contactform.component.html',
-  styleUrl: './contactform.component.scss'
+  styleUrl: './contactform.component.scss',
 })
 export class ContactformComponent {
-
+  onSubmit() {
+    console.log('funktioniert');
+  }
 }
