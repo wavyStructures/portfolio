7dbcec97488bde541b672577772196f406b08f67
wavyStructures <anja_schwab@gmx.de>
2024-09-04 07:19:28 +0200

    contactform and HttpClient

---
+++ src/app/main-page/contactform/contactform.component.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-contactform',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './contactform.component.html',
+  styleUrl: './contactform.component.scss',
+})
+export class ContactformComponent {
+  http = inject(HttpClient);
+
+  contactData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+  };
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+      // this.emptyForm();
+    }
+  }
+
+  // emptyForm() {
+  //   this.contactData.firstName = '';
+  //   this.contactData.lastName = '';
+  //   this.contactData.email = '';
+  //   this.contactData.message = '';
+  // }
+}
---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactformComponent } from './contactform/contactform.component';
+
+@Component({
+  selector: 'app-main-page',
+  standalone: true,
+  imports: [
+    AboveTheFoldComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    ProjectsComponent,
+    ContactformComponent,
+  ],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.scss',
+})
+export class MainPageComponent {}
