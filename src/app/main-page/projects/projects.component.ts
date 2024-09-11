bfd12de65b923aee6e3ccadc2cd2efc04f4c3fec
wavyStructures <anja_schwab@gmx.de>
2024-09-11 15:56:11 +0200

    projects first commit

--- src/app/main-page/projects/projects.component.ts
+++ src/app/main-page/projects/projects.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ResponsiveService } from '../../responsive-services/responsive.service';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet, TranslateModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {
-
-}
+export class ProjectsComponent {}
