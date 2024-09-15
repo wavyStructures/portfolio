5266ea21715d2b17f2b7052ef60d8b0680d91895
wavyStructures <anja_schwab@gmx.de>
2024-09-15 12:29:59 +0200

    about arrow ngClass scrolled

--- src/app/main-page/about-me/about-me.component.ts
+++ src/app/main-page/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ResponsiveService } from '../../responsive-services/responsive.service';
 import { Subscription } from 'rxjs';
 import { RouterLink, RouterOutlet } from '@angular/router';
@@ -7,7 +8,7 @@ import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, TranslateModule],
+  imports: [CommonModule, RouterLink, RouterOutlet, TranslateModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
 })
@@ -15,6 +16,14 @@ export class AboutMeComponent implements OnInit, OnDestroy {
   isMobile: boolean = false;
   isTablet: boolean = false;
   isDesktop: boolean = false;
+
+  isScrolled = false;
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // You can modify this logic as per your needs
+    const offset = window.pageYOffset || document.documentElement.scrollTop;
+    this.isScrolled = offset > 100; // Change 100 to the value when you want the arrow to move
+  }
 
   subscription: Subscription = new Subscription();
 
