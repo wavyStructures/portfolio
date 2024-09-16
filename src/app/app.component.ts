2adff4f9f96d9a9793c049243c7c7182ec3ec7c9
wavyStructures <anja_schwab@gmx.de>
2024-09-16 17:04:58 +0200

    aos into app.component.ts and other

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { TranslateModule } from '@ngx-translate/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, TranslateModule],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    MainPageComponent,
+    FooterComponent,
+    TranslateModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Anja Schwab';
+
+  ngOnInit(): void {
+    // Initialize AOS globally
+    AOS.init({
+      duration: 1200,
+      easing: 'ease-in-out',
+      once: true,
+    });
+  }
 }
