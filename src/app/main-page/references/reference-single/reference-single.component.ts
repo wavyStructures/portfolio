9883c3f4502f2d2142b0046c87310ba3ef8bdda4
wavyStructures <anja_schwab@gmx.de>
2024-09-18 07:50:07 +0200

    references and reference-single implementing

--- src/app/main-page/references/reference-single/reference-single.component.ts
+++ src/app/main-page/references/reference-single/reference-single.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-reference-single',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './reference-single.component.html',
   styleUrl: './reference-single.component.scss',
 })
 export class ReferenceSingleComponent {
-
-
-  
+  @Input() references!: {
+    name: string;
+    project: string;
+    opinion: string;
+  };
 }
--- src/app/main-page/references/references.component.ts
+++ src/app/main-page/references/references.component.ts
@@ -1,33 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ResponsiveService } from '../../responsive-services/responsive.service';
 import { Subscription } from 'rxjs';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { Reference } from '../../interfaces/reference.interface';
-import { ReferenceSingleComponent } from "./reference-single/reference-single.component";
+import { ReferenceSingleComponent } from './reference-single/reference-single.component';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, TranslateModule, ReferenceSingleComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterOutlet,
+    TranslateModule,
+    ReferenceSingleComponent,
+  ],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss',
 })
 export class ReferencesComponent {
-  references: Reference[] = [
+  references = [
     {
       name: 'Stefanie V.',
-      project: 'PEOPLE_STATUS_1',
+      project: 'PEOPLE_1',
       opinion: 'REFERENCE_1',
     },
     {
       name: 'Sebastian S.',
-      project: 'PEOPLE_STATUS_2',
+      project: 'PEOPLE_2',
       opinion: 'REFERENCE_2',
     },
     {
       name: 'Mario W.',
-      project: 'PEOPLE_STATUS_3',
+      project: 'PEOPLE_3',
       opinion: 'REFERENCE_3',
     },
   ];
