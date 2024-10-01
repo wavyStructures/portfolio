07878a648a1d2989e2c71e0631004784b36c2cce
wavyStructures <anja_schwab@gmx.de>
2024-10-01 12:16:28 +0200

    imprint link footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [{ path: '', component: MainPageComponent }];
+export const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+];
---
+++ src/app/imprint/imprint.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+
+@Component({
+  selector: 'app-imprint',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './imprint.component.html',
+  styleUrl: './imprint.component.scss'
+})
+export class ImprintComponent {
+}
